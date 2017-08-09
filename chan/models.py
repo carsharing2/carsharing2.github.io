8cafb92bf19505e51f7266e51bb052496d7539c6
Lagonee <vasstog@gmail.com>
2017-08-09 15:57:58 +0500

    first commit

---
+++ chan/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class Post(models.Model):
+    message = models.CharField(max_length = 1000)
+    mail = models.CharField(max_length = 100, blank = True, null = True)
+    date = models.DateTimeField('Post date')
+    sage = models.BooleanField('Sage')
+    parent_thread = models.PositiveIntegerField('Parent', blank = True, null = True)
+    ip = models.CharField(max_length = 20, blank = True, null = True)
+    post_id = models.IntegerField(primary_key = True)
+
+    def __str__(self):
+        return self.message
+
+class Users_base(models.Model):
+    ip = models.CharField(max_length = 20, unique=True)
+    last_post_date = models.DateTimeField('Last post date')
+    ban_reason = models.CharField(max_length = 500, blank = True, null = True)
+    ban_expire = models.DateTimeField('Expire date', blank = True, null = True)
+
+    def is_banned(self):
+        from django.utils import timezone
+        if self.ban_reason is not None:
+            if self.ban_expire and (self.ban_expire > timezone.now()): 
+                return True
+            else: #ban expired
+                self.ban_reason = None
+                self.save()
+                return False
+
+    def __str__(self):
+        return self.ip
---
+++ chan/views.py
@@ -0,0 +1,104 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.utils import timezone
+from .models import Post, Users_base
+from math import ceil
+import datetime
+
+def index(request, page=0):
+    t_on_page = 3
+    page = int(page)
+    posts = Post.objects.filter(parent_thread = None)
+    if posts.count() <= t_on_page * page:
+        raise Http404('Page does not exist')
+
+    pages_total_count = ceil(posts.count() / t_on_page)
+    pages_total = list(range(pages_total_count))
+    
+    for p in posts:
+        try:
+            p.last_post_id = Post.objects.filter(parent_thread = p.post_id, sage = False).last().post_id
+        except AttributeError: #no posts in thread
+            p.last_post_id = p.post_id
+
+    posts = sorted(list(posts), key = lambda p: p.last_post_id, reverse=True) #sort threads py last post id
+
+    data = []
+    for p in posts[page*t_on_page:page*t_on_page + t_on_page]:
+        data.append({
+            'op': p,
+            'related_posts': Post.objects.filter(parent_thread = p.post_id).order_by('-post_id')[:3:-1],
+            })
+
+    try:
+        message = request.session.pop('message')
+    except KeyError:
+        message = None
+
+    return render(request, 'chan/index.html', {'posts': data, 'pages': pages_total, 'message': message})
+    
+def create(request, thread_id=None):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+
+    sage = True if 'sage' in request.POST else False
+
+
+    try: 
+        user = Users_base.objects.get(ip=ip)
+        new_user = False
+    except Users_base.DoesNotExist:
+        user = Users_base.objects.create(
+            ip = ip,
+            last_post_date = timezone.now(),
+            ban_reason = None,
+            )
+        new_user = True
+
+    if user.is_banned():
+        allow_post = False
+        message = 'Ban: {}; expires: {}'.format(user.ban_reason, user.ban_expire.ctime())
+    elif (timezone.now() - user.last_post_date < datetime.timedelta(seconds = 5)) and not new_user:
+        allow_post = False
+        message = 'You post too fast'
+    else:
+        allow_post = True
+        user.last_post_date = timezone.now()
+        user.save()
+    
+
+    if allow_post:
+        post_data = Post.objects.create(
+            message = request.POST['message'],
+            mail = request.POST['mail'],
+            date = timezone.now(),
+            sage = sage, 
+            ip = ip,
+            parent_thread = thread_id,
+            )
+        message = 'Post send'
+
+    request.session['message'] = message
+    
+    if thread_id is None: #If user created new thread.
+        threads = Post.objects.filter(parent_thread = None)
+        if threads.count() > 10: #Maximum number of threads on the board.
+            last_thread_id = threads.order_by('post_id').first().post_id
+            threads.order_by('post_id').first().delete() #delete last thread
+            Post.objects.filter(parent_thread = last_thread_id).delete() #delete posts in last thread
+        return redirect('index')
+    else:
+        return redirect('thread', thread_id = thread_id)
+    
+
+def thread(request, thread_id):
+    op_post = get_object_or_404(Post, post_id = thread_id, parent_thread = None)
+    posts = Post.objects.filter(parent_thread = (thread_id))
+    try:
+        message = request.session.pop('message')
+    except KeyError:
+        message = None
+    return render(request, 'chan/thread.html', {'op_post': op_post, 'posts': posts, 'message': message})
